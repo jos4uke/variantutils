0b144e37e865ac6a3968107fc200eccc2975fa0b
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2014-06-18 11:18:14 +0200

    add a general method to count a given primary tag avoiding duplicating count method

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -33,6 +33,28 @@ setMethod(
 			 }
 		  })
 
+setMethod(
+		  f = "countPrimaryTag",
+		  signature = "BamCounter",
+		  definition = function(object, primaryTag="IE", tags){
+			if (!(is.null(tags))) {
+                 if (!(class(primaryTag)=="character") && !(nchar(primaryTag)==2))
+                    stop("provided 'primaryTag' value is not a 2-character string")
+                 if (!(is(tags,"character") && any(nchar(tags)>0)))
+                    stop("tags must be a character vector and each tag element must be non-empty string")
+				 if (is.null(object@res[["tag"]][[primaryTag]]))
+					stop(paste(primaryTag, "tag does not exist in BamCounter object", sep=" "))
+
+				 # cross primaryTag with given other tags
+				 df <- as.data.frame(xtabs(as.formula(paste(paste("~",primaryTag,sep=""),tags, sep="+",collapse="+")), data=object@res[["tag"]]))
+				 df
+			} else {
+				# only primaryTag
+				df <- as.data.frame(xtabs(paste("~",primaryTag,sep=""), data=object@res[["tag"]]))
+				df
+			}
+		  })
+
 setReplaceMethod(
 				 f = "setCounts",
 				 signature = "BamCounter",
